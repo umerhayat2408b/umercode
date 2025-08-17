30693c763d7f8d957fb8bce9cba2d65f60a4dfa6
umerhayat2408b <umerhayat2408b@aptechgdn.net>
2025-08-17 17:17:31 +0000

    Added jsbasic/2.js

---
+++ jsbasic/2.js
@@ -0,0 +1,13 @@
+// let a = Math.ceil ( Math.random() * 3) + 1 ;
+
+// console.log(a);
+
+
+let mydate = new Date();
+
+console.log(mydate.todateString());
+
+
+// let array = (309,839,umer,false);
+
+// console.log(array);
